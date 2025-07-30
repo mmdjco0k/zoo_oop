7f27a2cb565019f074f7a3be7ab95ed680aa2602
mmdjcook <mmdjcook@kali.mmdjcook>
2025-07-30 11:55:48 -0400

    create lion,rat and snake classes

--- animal.py
+++ animal.py
@@ -53,9 +53,69 @@ class Animal(ABC):
     def sleep(self):
         return f"The {self.name} is sleeping"
     
-
+    
     def info(self):
         print(f"name is {self.name}")
         print(f"age is {self._age}")
         print(f"weight is {self._weight}")
+    
+    def make_sound(self):
+        pass
+
+class lion(Animal):
+    def __init__(self , name:str , weight:float , age:int , taile_size:float ):
+        super().__init__(name, weight, age)
+        self._taile_size = taile_size
+    
+    @property
+    def taile_size(self):
+        return f"The size of the {self.name} tail is {self._taile_size} cm"
+
+    @taile_size.setter
+    def taile_size(self , new_size):
+        if new_size < 0 :
+            raise Exception("tail size cannot be negative")
+        self._taile_size = new_size
+    
+    def make_sound(self):
+        return "Roar"
+    
+    def info(self):
+        super().info()
+        print(f"The size of the {self.name} tail is {self._taile_size} cm")
+
+class rat(Animal):
+    def __init__(self , name:str , weight:float , age:int , color:str ):
+        super().__init__(name, weight, age)
+        self._color = color
+    
+    @property
+    def color(self):
+        return f"The color of {self.name} is {self._color} "
+    
+    def make_sound(self):
+        return "hisses"
+    
+    def info(self):
+        super().info()
+        print(f"The color of {self.name} is {self._color} ")
+
+class snake(Animal):
+    def __init__(self , name:str , weight:float , age:int ,venomous: bool ):
+        super().__init__(name, weight, age)
+        self._venomous = venomous
+    
+    @property
+    def venomous(self):
+        if self._venomous == True:
+            return f"{self.name} is venomous"
+        else:
+            return f"{self.name} is not venomous"
+    
+    def make_sound(self):
+        return "sisss"
+    
+    def info(self):
+        super().info()
+        print(f"{self.name} is {'venomous' if self._venomous else 'not venomous'}")
 
